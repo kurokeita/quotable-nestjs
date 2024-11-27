ebb05cabd3e5d218e345bc580c126f7cec0f7229
kurokeita <kuro.keita94@gmail.com>
2024-11-27 20:18:59 +0700

    fix the `HAVING` condition breaking postgres

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory, {
+  SwaggerModule.setup('/', app, documentFactory, {
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.js',
